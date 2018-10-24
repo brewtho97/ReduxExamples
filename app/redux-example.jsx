bdcac7282f3c36f9e2211d202d9440b20520e43b
Thomas Brewer <brewtho@nlg.nhs.uk>
2018-10-24 12:23:12 +0100

    Map Reducer and action generators added

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 const redux = require('redux');
+const axios = require('axios');
 
 console.log('Starting redux example...');
 
@@ -90,10 +91,53 @@ let removeMovie = (id) => {
   };
 }
 
+//MOVIE REDUCER AND ACTION GENERATOR
+//---------------------------------
+let mapReducer = (state = {isFetching: false, url:undefined}, action) => {
+  switch(action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+  }
+};
+
+let startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  };
+};
+
+let completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  };
+};
+
+let fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+  axios.get('http://ipinfo.io').then((response) => {
+    let loc = response.data.loc;
+    let baseURL = 'http://maps.google.com?q=';
+
+    store.dispatch(completeLocationFetch(baseURL + loc));
+  })
+}
+
 let reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 });
 
 let store = redux.createStore(reducer, redux.compose(
@@ -102,18 +146,27 @@ let store = redux.createStore(reducer, redux.compose(
 
 let unsubscribe = store.subscribe(() => {
   let state = store.getState();
-  console.log('New Name: ', state.name);
   console.log('New State: ', store.getState());
-  document.getElementById('app').innerHTML = state.name;
-});
 
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...'
+  } 
+  else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View Your Location</a>'
+  }
+});
+//unsubscribe(); //Stops the subscribe function from running on store changes
+
+fetchLocation();
 //Sends off an action, reducer has to handle the action.
 store.dispatch(changeName('Thomas')); 
+
 store.dispatch(addHobby('Programming'));
 store.dispatch(addHobby('Reading'));
 store.dispatch(removeHobby(2));
-//unsubscribe(); //Stops the subscribe function from running on store changes
+
 store.dispatch(changeName('Kim'));
+
 store.dispatch(addMovie('Scott Pilgrim vs the World', 'Comedy'));
 store.dispatch(addMovie('Whiplash', 'Drama'));
 store.dispatch(removeMovie(1));
